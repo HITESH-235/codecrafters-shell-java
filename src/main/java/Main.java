942faac287df3dcca3b872aec17e7750aa4561be
Hitesh Sinha <hiteshsinha2007@gmail.com>
2026-06-17 19:42:00 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,27 +10,33 @@ public class Main {
             String[] parts = input.split(" ");
 
             if (parts[0].equals("echo")) {
-                for (int i=1; i<parts.length; i++) {
+                for (int i = 1; i < parts.length; i++) {
                     System.out.print(parts[i]);
-                    System.out.print((i < parts.length-1) ? " ":"");
+                    if (i < parts.length - 1) {
+                        System.out.print(" ");
+                    }
                 }
                 System.out.println();
             }
             else if (parts[0].equals("type")) {
                 String curr = parts[1];
-                if (curr.equals("echo") || curr.equals("exit") || curr.equals("type")) {
-                    System.out.println(curr+" is a shell builtin");
-                }
-                else {
-                    System.out.println(curr+": not found");
+
+                if (curr.equals("echo") ||
+                    curr.equals("exit") ||
+                    curr.equals("type")) {
+
+                    System.out.println(curr + " is a shell builtin");
+                } else {
+                    System.out.println(curr + ": not found");
                 }
             }
             else if (parts[0].equals("exit")) {
-                sc.close();
                 break;
             }
-            else System.out.println(input+": command not found");    
-            // sc.close();   
+            else {
+                System.out.println(parts[0] + ": command not found");
+            }   
         }
+        sc.close();
     }
 }
